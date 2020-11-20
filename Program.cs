6f810968e3d7f41b365d2702b04469f3d7f68e39
Justice Mabuza <jmabuza@my365-uwc.ac.za>
2020-11-20 08:54:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+namespace Assignment1.p2
+{
+    class Program
+    {
+        class ReverseString
+        {
+
+            static void Main(string[] args)
+            {
+                string str = "";
+                Console.WriteLine("Enter The String");
+                string s = Console.ReadLine();
+
+                int len = s.Length;
+
+                for (int j = len - 1; j >= 0; j--)
+                {
+                    str = str + s[j];
+                }
+                if (str == s)
+                    Console.WriteLine(s + " is palindrome");
+
+                else
+                    Console.WriteLine(s + " is not palindrome");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
